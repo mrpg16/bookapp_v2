dd02d4c649f8efa02c9d73e690ea65ceff0194f2
Igor Zimin <igor.zimin@abbyy.com>
2024-09-23 16:26:29 +0200

    iter n3

--- src/main/java/prod/bookapp/dto/UserAuthResponseDTO.java
+++ src/main/java/prod/bookapp/dto/UserAuthResponseDTO.java
@@ -1,14 +1,12 @@
 package prod.bookapp.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class UserAuthResponseDTO {
     private String accessToken;
     private String refreshToken;
